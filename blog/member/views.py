01ba766cd4fd49698e5e8c90a5adc05f050d216d
han-son-03 <rkdgksths11@naver.com>
2025-09-18 15:24:08 +0900

    ✨ Make signup views, html

--- blog/member/views.py
+++ blog/member/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def signup(request):
+    # username = request.POST.get('username')
+    # password1 = request.POST.get('password1')
+    # password2 = request.POST.get('password2')
+    #
+    # print('username', username)
+    # print('password1', password1)
+    # print('password2', password2)
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/accounts/login/')
+    else:
+        form = UserCreationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'registration/signup.html', context)
