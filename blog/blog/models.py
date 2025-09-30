1edc3f6da8b5b121b62e925964a425d2715b395d
han-son-03 <rkdgksths11@naver.com>
2025-09-30 17:28:12 +0900

    ✨ 댓글 기능 추가

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -2,9 +2,11 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
+from utils.models import TimeStampedModel
+
 User = get_user_model()
 
-class Blog(models.Model):
+class Blog(TimeStampedModel):
     CATEGORY_CHOICES = (
     ('free', '자유'),
     ('travel', '여행'),
@@ -17,8 +19,7 @@ class Blog(models.Model):
     content = models.TextField('본문')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    created_at = models.DateTimeField('작성일자', auto_now_add=True)
-    updated_at = models.DateTimeField('수정일자', auto_now=True)
+
 
     def __str__(self):
         return f'[{self.get_category_display()}] {self.title[:10]}'
@@ -36,3 +37,21 @@ class Blog(models.Model):
 # 수정일자 ✅
 # 카테고리✅
 
+class Comment(TimeStampedModel):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    content = models.CharField('본문', max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.blog.title} 댓글'
+
+    class Meta:
+        verbose_name = '댓글'
+        verbose_name_plural = '댓글 목록'
+
+
+#블로그 정도
+#댓글 내용
+#작성자
+#작성일자
+#수정일자
