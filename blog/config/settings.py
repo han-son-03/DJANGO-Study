6139ef4154a1776e4afb67cebc1aeab83f95f26b
han-son-03 <rkdgksths11@naver.com>
2025-10-01 16:27:49 +0900

    ✨ Summernote_config 수정

--- blog/config/settings.py
+++ blog/config/settings.py
@@ -164,30 +164,31 @@ SUMMERNOTE_CONFIG = {
 
         # Change editor size
         'width': '100%',
-        'height': '480',
+        'height': '600',
 
         # Use proper language setting automatically (default)
 
         # Toolbar customization
         # https://summernote.org/deep-dive/#custom-toolbar-popover
         'toolbar': [
-            ['style', ['style']],
-            ['font', ['bold', 'underline', 'clear']],
+            ['style', ['style', 'italic', 'underline', 'clear']],
+            ['font', ['bold', 'underline', 'clear', 'strikethrough', 'superscript']],
+            ['fontsize', ['fontsize']],
             ['fontname', ['fontname']],
             ['color', ['color']],
             ['para', ['ul', 'ol', 'paragraph']],
-            ['table', ['table']],
-            ['insert', ['link', 'picture', 'video']],
-            ['view', ['fullscreen', 'codeview', 'help']],
+            # ['table', ['table']],
+            ['insert', ['link', 'picture']],
+            ['view', ['fullscreen', 'help']],
         ],
 
         # Or, explicitly set language/locale for editor
         'lang': 'ko-KR',
 
         # You can also add custom settings for external plugins
-        'print': {
-            'stylesheetUrl': '/some_static_folder/printable.css',
-        },
+        # 'print': {
+        #     'stylesheetUrl': '/some_static_folder/printable.css',
+        # },
         'codemirror': {
             'mode': 'htmlmixed',
             'lineNumbers': 'true',
