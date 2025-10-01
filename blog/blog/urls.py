9b6efbd14f6e7724b2abe1abccc2ed2879f580a4
han-son-03 <rkdgksths11@naver.com>
2025-10-01 18:31:47 +0900

    이미지 썸내일 만들기 중간 커밋

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,8 +1,7 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
 from blog import cb_views
+from blog import views
 
 app_name = 'blog'
 
@@ -12,6 +11,7 @@ urlpatterns = [
     path('<int:blog_pk>/', cb_views.BlogDetailView.as_view(), name='detail'),
     path('create/', cb_views.BlogCreateView.as_view(), name='create'),
     path('<int:pk>/update', cb_views.BlogUpdateView.as_view(), name='update'),
+    # path('<int:pk>/update', views.blog_update, name='update'),
     path('<int:pk>/delete', cb_views.BlogDeleteView.as_view(), name='delete'),
 
     path('comment/create/<int:blog_pk>/', cb_views.CommentCreateView.as_view(), name='comment_create'),
