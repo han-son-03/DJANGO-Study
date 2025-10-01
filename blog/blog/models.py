788d7ab5a1e515a714abf600daa0ceea32ee4da0
han-son-03 <rkdgksths11@naver.com>
2025-10-01 23:32:12 +0900

    ✨ Thumbnail image 표시
    models.py에 썸내일 저장 함수 정의 blog_List에 표시 되는 이미지 thumbnail 파일로 표시

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,3 +1,7 @@
+from io import BytesIO
+from pathlib import Path
+
+from PIL import Image
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
@@ -20,6 +24,8 @@ class Blog(TimeStampedModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     image = models.ImageField('이미지', null=True, blank=True, upload_to='blog/%Y/%m/%d')
+    thumbnail = models.ImageField('썸네일', null=True, blank=True,
+                                  upload_to='blog/%Y/%m/%d/thumbnail')
     # 2024/4/23일
     # blog/2024/4/23/이미지파일.jpg
     # ImageField, FileField 와 같기만 이미지만 업로드하게 되어있다.
@@ -29,8 +35,49 @@ class Blog(TimeStampedModel):
 
     def __str__(self):
         return f'[{self.get_category_display()}] {self.title[:10]}'
+
+    def get_thumbnail_image(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        elif self.image:
+            return self.image.url
+        return None
+
+
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'blog_pk':self.pk})
+
+    def save(self, *args, **kwargs):
+        if not self.image:
+            return super().save(*args, **kwargs)
+
+        image = Image.open(self.image)
+        image.thumbnail((300, 300))
+
+        image_path = Path(self.image.name) # Path 라이브러리를 사용해 이미지 경로를 가져옵니다
+
+        thumbnail_name = image_path.stem # /blog/2024/8/13/database.png -> database
+        thumbnail_extension = image_path.suffix # /blog/2024/8/13/database.png -> .png
+        thumbnail_filename = f'{thumbnail_name}_thumb{thumbnail_extension}'
+
+        if thumbnail_extension in ['.jpg', 'jpeg']:
+            file_type = 'JPEG'
+        elif thumbnail_extension == '.gif':
+            file_type = 'GIF'
+        elif thumbnail_extension == '.png':
+            file_type = 'PNG'
+        else:
+            return super().save(*args, **kwargs)
+
+        temp_thumb = BytesIO()
+        image.save(temp_thumb, file_type)
+        temp_thumb.seek(0)
+
+        self.thumbnail.save(thumbnail_filename, temp_thumb, save=False)
+        temp_thumb.close()
+        return super().save(*args, **kwargs)
+
+
 
     class Meta:
         verbose_name = '블로그'
